1b1216968a44d53f02b8732111a3d74f15f94f5c
Aban Zubair <abanzubair@gmail.com>
2026-03-25 13:03:22 +0530

    Completed first part of landing page

--- src/components/hero_text/herotext.jsx
+++ src/components/hero_text/herotext.jsx
@@ -8,9 +8,15 @@ const Herotext = () => {
                 The <i>Poetry</i> of the Pour
             </h1>
             <h2>A curated space where literary depth meets artisanal precision. Every cup is a chapter, every visit an essay in stillness.</h2>
+
+            <div className="btns">
             <div className="explr-btn">
-                <button>Explore The Menu</button>
+                <button>EXPLORE THE MENU</button>
                 </div>
+            <div className="philosophy-btn">
+                <button>OUR PHILOSOPHY<div className="arrow">→</div></button>
+                </div>
+            </div>
         </div>
     )
 }
